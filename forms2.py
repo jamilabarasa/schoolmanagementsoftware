3a07769059967c5d1b08d0fa56c65896c21ff723
mila <jamilabarasa18@gmail.com>
2020-05-12 13:29:48 +0300

    removed forms2.py

--- forms2.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField
-from wtforms.validators import DataRequired,Email
-
-class Teacherlogin(FlaskForm):
-    email = StringField('Email',validators=[Email(),DataRequired(),])
-    password = PasswordField('Password',validators=[DataRequired(),])
-    
